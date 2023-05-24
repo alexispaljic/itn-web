ddad8b48144b1c18c4b9a3545365738949714546
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2023-05-24 15:16:24 +0200

    💚 Patch hrefs for gh-pages

---
+++ patch-hrefs-for-gh-pages.py
@@ -0,0 +1,11 @@
+
+from pathlib import Path
+
+ROOT = "https://boisgera.github.io/itn-web/"
+
+for path in Path("dist").rglob("*.html"):
+    with path.open(mode="r", encoding="utf-8") as f:
+        src = f.read()
+    src = src.replace('href="/', f'href="https:{ROOT}')
+    with path.open(mode="w", encoding="utf-8") as f:
+        f.write(src)
