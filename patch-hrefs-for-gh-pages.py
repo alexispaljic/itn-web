3dd13fae0bfc0ea0d8456df49ae3685bdc2cbab2
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2023-05-24 21:52:12 +0200

    💚 Fix srcset links

--- patch-hrefs-for-gh-pages.py
+++ patch-hrefs-for-gh-pages.py
@@ -7,5 +7,6 @@ for path in Path("dist").rglob("*.html"):
     with path.open(mode="r", encoding="utf-8") as f:
         src = f.read()
     src = src.replace('href="/', f'href="{ROOT}/')
+    src = src.replace('srcset="/', f'srcset="{ROOT}/')
     with path.open(mode="w", encoding="utf-8") as f:
         f.write(src)
