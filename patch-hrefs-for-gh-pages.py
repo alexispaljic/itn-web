57e9bc010fa8c57295ef2a17525ca8acf6cb9ce9
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2023-05-25 09:27:00 +0200

    💚 Patch links for gh-pages

--- patch-hrefs-for-gh-pages.py
+++ patch-hrefs-for-gh-pages.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import re
 
 ROOT = "https://boisgera.github.io/itn-web"
 
@@ -8,6 +9,15 @@ for path in Path("dist").rglob("*.html"):
         src = f.read()
     src = src.replace('href="/', f'href="{ROOT}/')
     src = src.replace('src="/', f'src="{ROOT}/')
-    # TODO: srcset
+    while match := re.search(r'srcset="/[^"]*"', src): # ⚠️ Hack
+        content = match.group(0)
+        start, end = match.span(0)
+        prefix, string = content[:7], content[7:]
+        assert prefix == "srcset="
+        srcset_paths = string[1:-1].split(",")
+        #print(srcset_paths)
+        srcset_paths = [f"{ROOT}{p}" for p in srcset_paths if p.startswith("/")]
+        srcset =  f'srcset="{" ".join(srcset_paths)}"'
+        src = src[:start] + srcset + src[end:]
     with path.open(mode="w", encoding="utf-8") as f:
         f.write(src)
